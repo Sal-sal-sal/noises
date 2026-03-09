a64363b8c362dca8e95736fe554b078c11bb9cf4
Sal-sal-sal <ccgtru@gmail.com>
2026-03-10 04:25:10 +0500

    Add noises as normal folder

---
+++ noises/app/noises/page.tsx
@@ -0,0 +1,11 @@
+import Noises from "@/components/noises/noises";
+
+export default function NoisesPage() {
+  return (
+    <>
+    <div className="flex min-h-screen items-center justify-center font-sans  h-full w-full bg-amber-300 z-[-3] ">
+      <Noises />
+      </div>
+    </>
+  );
+}
